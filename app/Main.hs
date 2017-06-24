ddb846dda6d9da29cfd68f6ed4477a33851ea3d9
Philipp Middendorf <pmidden@secure.mailbox.org>
2017-06-24 14:41:52 +0200

    add exclusions

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,40 @@
 module Main where
 
-import           Control.Monad      (Monad, filterM, join, liftM, mapM, return,
-                                     (>>=))
-import           Data.Bool          (Bool, otherwise)
-import           Data.Char          (intToDigit)
-import           Data.Foldable      (foldMap)
-import Control.Exception(IOException,catch)
-import           Data.Function      (($), (.))
-import           Data.Functor       ((<$>))
-import           Options.Applicative            (Parser, execParser,switch,
-                                                 fullDesc, header, help, helper,
-                                                 info, long, metavar,
-                                                 progDesc, strOption, (<**>))
-import           Data.Int           (Int)
-import           Data.List          (concatMap, length, replicate, unlines)
-import           Data.Monoid        (Monoid (..), (<>))
-import           Data.Ord           (max, (<))
-import           Data.String        (String)
-import           Data.Time.Calendar (Day, addGregorianMonthsClip, toGregorian)
-import           Data.Time.Clock    (getCurrentTime, utctDay)
-import           Numeric            (showIntAtBase)
-import           PlpdMailTemplate   (printMailTemplate)
-import           Prelude            (fromIntegral, (-))
-import           System.Directory   (copyFile, createDirectory,
-                                     doesDirectoryExist, doesFileExist,
-                                     listDirectory, removeDirectoryRecursive)
+import           Control.Exception   (IOException, catch)
+import           Control.Monad       (Monad, filterM, join, liftM, mapM, return,
+                                      (>>=))
+import           Data.Bool           (Bool, otherwise)
+import           Data.Char           (intToDigit)
+import           Data.Foldable       (foldMap)
+import           Data.Function       (($), (.))
+import           Data.Functor        ((<$>))
+import           Data.Int            (Int)
+import           Data.List           (concatMap, length, replicate, unlines,notElem,filter)
+import           Data.Monoid         (Monoid (..), (<>))
+import           Data.Ord            (max, (<))
+import           Data.String         (String)
+import           Data.Time.Calendar  (Day, addGregorianMonthsClip, toGregorian)
+import           Data.Time.Clock     (getCurrentTime, utctDay)
+import           Numeric             (showIntAtBase)
+import           Options.Applicative (Parser, execParser, fullDesc, header,
+                                      help, helper, info, long, many, metavar,
+                                      progDesc, strOption, switch, (<**>))
+import           PlpdMailTemplate    (printMailTemplate)
+import           Prelude             (fromIntegral, (-))
+import           System.Directory    (copyFile, createDirectory,
+                                      doesDirectoryExist, doesFileExist,
+                                      listDirectory, removeDirectoryRecursive)
 
-import           System.FilePath    (FilePath, (</>))
-import           System.IO          (IO, print, putStrLn)
-import           System.Posix.Files (setFileMode, setOwnerAndGroup,
-                                     unionFileModes)
-import qualified System.Posix.Files as PosixFiles
-import           System.Posix.Types (FileMode)
-import           System.Posix.User  (getGroupEntryForName, getUserEntryForName,
-                                     groupID, userID)
-import           Text.Show          (Show (..))
-import Control.Applicative((<*>))
+import           Control.Applicative ((<*>))
+import           System.FilePath     (FilePath, (</>))
+import           System.IO           (IO, print, putStrLn)
+import           System.Posix.Files  (setFileMode, setOwnerAndGroup,
+                                      unionFileModes)
+import qualified System.Posix.Files  as PosixFiles
+import           System.Posix.Types  (FileMode)
+import           System.Posix.User   (getGroupEntryForName, getUserEntryForName,
+                                      groupID, userID)
+import           Text.Show           (Show (..))
 
 {-
 Beschreibung des Programms:
@@ -118,11 +117,12 @@ instance Show FsOperation where
   show (FsChmod p flags) = "chmod " <> quote p <> " " <> show flags
 
 data CliVars = CliVars {
-    cliSourceDir :: FilePath
-  , cliTargetDir :: FilePath
-  , cliUser      :: String
-  , cliGroup      :: String
-  , cliWetRun    :: Bool
+    cliSourceDir   :: FilePath
+  , cliTargetDir   :: FilePath
+  , cliUser        :: String
+  , cliGroup       :: String
+  , cliWetRun      :: Bool
+  , cliExcludeDirs :: [FilePath]
   }
 
 cliVarsParser :: Parser CliVars
@@ -147,6 +147,10 @@ cliVarsParser =
       <*> switch
           ( long "wet-run"
           <> help "really execute the actions (opposite of dry-run)")
+      <*> many ( strOption
+          ( long "exclude"
+         <> metavar "EXCLUSION"
+         <> help "Exclude the following directories from copying" ) )
 
 readCliVars :: IO CliVars
 readCliVars = execParser opts
@@ -194,9 +198,9 @@ removeOld (FsTarget target) lastMy =
     cats <- dirsBelowJustName target
     return (categoryRmOp <$> cats)
 
-copyNew :: FsSource -> FsTarget -> MonthYear -> IO [FsOperation]
-copyNew (FsSource source) (FsTarget target) currentMy = do
-  sourceCats <- dirsBelowJustName source
+copyNew :: FsSource -> FsTarget -> [FilePath] -> MonthYear -> IO [FsOperation]
+copyNew (FsSource source) (FsTarget target) exclusions currentMy = do
+  sourceCats <- filter (`notElem` exclusions) <$> (dirsBelowJustName source)
   let currentMyStr = show currentMy
       dirFilter = doesDirectoryExist . (</> currentMyStr) . (source </>)
   filteredCats <- filterM dirFilter sourceCats
@@ -212,10 +216,10 @@ copyNew (FsSource source) (FsTarget target) currentMy = do
            return $ mkdir : (fileOp <$> catFiles)
   concatMapM categoryOp filteredCats
 
-copyAndMove :: FsSource -> FsTarget -> MonthYear -> MonthYear -> IO [FsOperation]
-copyAndMove source target currentMy lastMy = do
+copyAndMove :: FsSource -> FsTarget -> [FilePath] -> MonthYear -> MonthYear -> IO [FsOperation]
+copyAndMove source target exclusions currentMy lastMy = do
   deleteOps <- removeOld target lastMy
-  copyOps <- copyNew source target currentMy
+  copyOps <- copyNew source target exclusions currentMy
   return (copyOps <> deleteOps)
 
 listDirectoryIgnoreExns :: FilePath -> IO [FilePath]
@@ -263,7 +267,7 @@ interpretOperation op = do
     groupIdForName name = groupID <$> getGroupEntryForName name
     interpretOperation' (FsCopy (FsSource source) (FsTarget target)) = copyFile source target
     interpretOperation' (FsMkdir (FsTarget t)) = createDirectoryIgnoreExns t
-    interpretOperation' (FsRemoveDir t) = return ()--removeDirectoryRecursive t
+    interpretOperation' (FsRemoveDir t) = removeDirectoryRecursive t
     interpretOperation' (FsChown path (FsGroup groupName) (FsOwner ownerName)) = do
       user <- userIdForName ownerName
       group <- groupIdForName groupName
@@ -274,9 +278,10 @@ keepBooks :: CliVars -> IO ()
 keepBooks cliVars = do
   let source = FsSource (cliSourceDir cliVars)
       target = FsTarget (cliTargetDir cliVars)
+      exclusions = cliExcludeDirs cliVars
   cmy <- currentMonthYear
   lmy <- lastMonthYear
-  copyAndMoveOps <- copyAndMove source target cmy lmy
+  copyAndMoveOps <- copyAndMove source target exclusions cmy lmy
   if cliWetRun cliVars
     then do
       putStrLn "Copying and moving documents..."
