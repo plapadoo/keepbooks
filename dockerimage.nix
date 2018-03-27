caffc5afea8b78cf608e9e0f17efa990e4ff9444
Philipp Middendorf <middendorf@plapadoo.de>
2018-03-27 12:18:32 +0200

    Update license, cabal file, and pin nixpkgs

--- dockerimage.nix
+++ dockerimage.nix
@@ -1,5 +1,15 @@
 let
-  pkgs = import <nixpkgs> { };
+  bootstrap = import <nixpkgs> { };
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs-channels";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src { };
   thisPackage = pkgs.haskellPackages.callCabal2nix "keepbooks" ./. {};
 in
   pkgs.dockerTools.buildImage {
