bcee3b300ecf6a03ece3994167c5d41ca66bd23c
Philipp Middendorf <middendorf@plapadoo.de>
2018-07-02 14:29:22 +0200

    Fix recursive copying in package-arch

--- packaging/package-arch.sh
+++ packaging/package-arch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+shopt -s extglob
 
 mkdir -p packaging/src/keepbooks
-cp -R ./* packaging/src/keepbooks
+cp -R !(packaging) packaging/src/keepbooks
 cd packaging
 makepkg --noextract
 repo-add /home/gitlab-runner/repo.db.tar.gz ./*.xz
