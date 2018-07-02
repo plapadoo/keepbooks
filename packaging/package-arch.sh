adf9b72a26a1b25e1b2710cffc6b112ed490359f
Philipp Middendorf <middendorf@plapadoo.de>
2018-07-02 14:30:14 +0200

    package-arch: Don’t assume $HOME is /home/gitlab-runner (it’s not)

--- packaging/package-arch.sh
+++ packaging/package-arch.sh
@@ -7,4 +7,4 @@ mkdir -p packaging/src/keepbooks
 cp -R !(packaging) packaging/src/keepbooks
 cd packaging
 makepkg --noextract
-repo-add /home/gitlab-runner/repo.db.tar.gz ./*.xz
+repo-add "$HOME/repo.db.tar.gz" ./*.xz
